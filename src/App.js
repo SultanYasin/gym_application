523d90625d83a3355aa0546908cd4518267790ab
Sultan Yasin <sultan.yasin@utb.ecutbildning.se>
2023-01-12 15:03:51 +0100

    adding nav&home&footer

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Home from "./Pages/Home";
+import ExerciesDetail from "./Pages/ExerciseDetail";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <Box width="400px" sx={{width : {xl : '1488px'}}} m = 'auto' >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercies/:id" element={<ExerciesDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+}
+
+export default App;
