4782a5512e4d28d3a685ee69cc177adeeb2a31ff
Sultan Yasin <sultan.yasin@utb.ecutbildning.se>
2023-04-15 16:50:49 +0200

    new exercises component

--- server/index.js
+++ server/index.js
@@ -1,25 +1,11 @@
 const express = require("express");
+
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
-//middleware
 app.use(cors());
-app.use(express.json()); //req.body
-
-
-
-// varje http-metod tar in en end-point, samt en async function som tar in ett request- och ett respons-objekt
-// i de fall clienten förväntas skicka med någon input-data - hämtar jag det från (request-objektet).body, och sparar värdena först.
-// i de fall jag behöver info om en specifik item id som finns i url'en - hämtas  det från (request-objektet).params.id och sparar värdet först.
-// sedan skickar en sql-query till databasen (post, put, delete och get(specific) metoderna skickar med de sparade värdena från .body 
-//och/eller .params.id där de behövs) och efter queryn har körts klart 
-// sparas resultatet.
-// datan som ligger i resultatets .rows görs om till json och skickas tillbaka till client-sidan i response-objektet 
-
-
-//ROUTES//
-//create a todo 
+app.use(express.json());
 
 app.post("/todos", async (req, res) => {
   try {
@@ -32,12 +18,9 @@ app.post("/todos", async (req, res) => {
     res.json(newTodo.rows[0]);
   } catch (err) {
     console.error(err.message);
-    //ifall blev nåt fel -> clienten redirect:as till index.html
-    res.sendFile('./index.html');
+    res.sendFile("./index.html");
   }
 });
-
-//get all todos
 
 app.get("/todos", async (req, res) => {
   try {
@@ -45,12 +28,9 @@ app.get("/todos", async (req, res) => {
     res.json(allTodos.rows);
   } catch (err) {
     console.error(err.message);
-     //ifall blev nåt fel -> clienten redirect:as till index.html
-    res.sendFile('./index.html');
+    res.sendFile("./index.html");
   }
 });
-
-//get a todo
 
 app.get("/todos/:id", async (req, res) => {
   try {
@@ -59,13 +39,10 @@ app.get("/todos/:id", async (req, res) => {
 
     res.json(todo.rows[0]);
   } catch (err) {
-    //On Error
     console.error(err.message);
-    res.sendFile('./index.html');
+    res.sendFile("./index.html");
   }
 });
-
-//update a todo
 
 app.put("/todos/:id", async (req, res) => {
   try {
@@ -79,13 +56,10 @@ app.put("/todos/:id", async (req, res) => {
     res.json("Todo was updated!");
   } catch (err) {
     //On Error
-    console.error(err.message); 
-    res.sendFile('./index.html');
-    
+    console.error(err.message);
+    res.sendFile("./index.html");
   }
 });
-
-//delete a todo
 
 app.delete("/todos/:id", async (req, res) => {
   try {
@@ -95,10 +69,9 @@ app.delete("/todos/:id", async (req, res) => {
   } catch (err) {
     //On Error
     console.log(err.message);
-    res.sendFile('./index.html');
+    res.sendFile("./index.html");
   }
 });
-
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
